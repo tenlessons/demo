9d739a3cc3525b5c32541578c07cd9bb43856662
Ten <ten@revenantblue.com>
2015-11-24 14:28:06 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+	/*
+	 * Rounding Example:
+	 * 
+	var value = 0.1 + 0.2;
+	console.log(value);
+	value = value.toFixed(2);
+	console.log(value);
+	console.log(Math.round(value)); // Rounds to 0
+	console.log(Math.round(0.5)); // Rounds to 1
+	
+	*/
+	console.log(Math.ceil(0.1)); // Rounds to 1
+	console.log(Math.floor(0.9)); // Rounds to 0
+});
